1869472f6bd800b7d3f448312197dcef54f071ef
tevfiksirin <tevfiksirin@gmail.com>
2022-11-25 17:37:56 +0300

    canliyayin

--- .output/public/_nuxt/composables.6a52be1f.js
+++
@@ -1,1 +0,0 @@
-import{k as r}from"./entry.725a1e16.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
--- .output/public/_payload.js
+++ .output/public/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1669386504703}
+export default {data:{},prerenderedAt:1669387066874}
