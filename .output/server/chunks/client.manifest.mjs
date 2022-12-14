548ba6c59d0ed871a6b214cb8c31aa29ef15d360
tevfiksirin <tevfiksirin@gmail.com>
2022-12-14 17:55:12 +0300

    logo1

---
+++ .output/server/chunks/client.manifest.mjs
@@ -0,0 +1,135 @@
+const client_manifest = {
+  "assets/img/linetv.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "linetv.3894e13e.png",
+    "src": "assets/img/linetv.png"
+  },
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "file": "entry.88a05618.js",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "dynamicImports": [
+      "virtual:nuxt:C:/Users/dwork/Documents/GitHub/linetv/.nuxt/error-component.mjs"
+    ],
+    "css": [
+      "entry.3dd4e114.css"
+    ]
+  },
+  "entry.3dd4e114.css": {
+    "file": "entry.3dd4e114.css",
+    "resourceType": "style"
+  },
+  "virtual:nuxt:C:/Users/dwork/Documents/GitHub/linetv/.nuxt/error-component.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "file": "error-component.33817811.js",
+    "src": "virtual:nuxt:C:/Users/dwork/Documents/GitHub/linetv/.nuxt/error-component.mjs",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ]
+  },
+  "pages/canliyayin.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "canliyayin.e4f08882.js",
+    "src": "pages/canliyayin.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ]
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.ddc77798.js",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "assets": [
+      "linetv.3894e13e.png"
+    ]
+  },
+  "linetv.3894e13e.png": {
+    "file": "linetv.3894e13e.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "pages/programlar.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "programlar.91277abe.js",
+    "src": "pages/programlar.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ]
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "error-404.65235aae.js",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs",
+      "_composables.96689907.js"
+    ],
+    "css": []
+  },
+  "error-404.23f2309d.css": {
+    "file": "error-404.23f2309d.css",
+    "resourceType": "style"
+  },
+  "_composables.96689907.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "composables.96689907.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ]
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "error-500.82062342.js",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_composables.96689907.js",
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "css": []
+  },
+  "error-500.aa16ed4d.css": {
+    "file": "error-500.aa16ed4d.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "file": "error-404.23f2309d.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "file": "error-500.aa16ed4d.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "file": "entry.3dd4e114.css",
+    "src": "node_modules/nuxt/dist/app/entry.css"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
---
+++ .output/server/chunks/programlar.1b9447f4.mjs
@@ -0,0 +1,40 @@
+import { mergeProps, useSSRContext } from 'vue';
+import { ssrRenderAttrs } from 'vue/server-renderer';
+import { _ as _export_sfc } from './server.mjs';
+import 'ofetch';
+import 'hookable';
+import 'unctx';
+import 'ufo';
+import 'h3';
+import '@unhead/vue';
+import '@unhead/dom';
+import 'vue-router';
+import 'defu';
+import './node-server.mjs';
+import 'node-fetch-native/polyfill';
+import 'http';
+import 'https';
+import 'destr';
+import 'unenv/runtime/fetch/index';
+import 'scule';
+import 'ohash';
+import 'unstorage';
+import 'radix3';
+import 'node:fs';
+import 'node:url';
+import 'pathe';
+
+const _sfc_main = {};
+function _sfc_ssrRender(_ctx, _push, _parent, _attrs) {
+  _push(`<div${ssrRenderAttrs(mergeProps({ class: "flex items-center justify-center w-full h-screen bg-slate-500" }, _attrs))}><h1 class="text-4xl text-white">Yap\u0131m A\u015Famas\u0131nda ...</h1></div>`);
+}
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("pages/programlar.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+const programlar = /* @__PURE__ */ _export_sfc(_sfc_main, [["ssrRender", _sfc_ssrRender]]);
+
+export { programlar as default };
+//# sourceMappingURL=programlar.1b9447f4.mjs.map
